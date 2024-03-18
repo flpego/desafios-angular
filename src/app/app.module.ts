c890d0a78eda48fe8d55caa824fea855148e9e53
flpego <flaviopegoraro2@gmail.com>
2024-03-18 18:49:11 -0300

    version alpha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './desafio-1/toolbar/toolbar.component';
+import { MenuLateralComponent } from './desafio-1/menu-lateral/menu-lateral.component';
+import { MainContentComponent } from './desafio-1/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MenuLateralComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
